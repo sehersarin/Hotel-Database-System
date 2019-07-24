f43356fb9dbac9292f62d7e7768e907df52e06b1
sehersarin <43328744+sehersarin@users.noreply.github.com>
2019-07-23 23:04:29 -0400

    Update updateBooking.php

--- updateBooking.php
+++ updateBooking.php
@@ -1,24 +1,17 @@
 <h1>Update Booking</h1>
 
-
-
 <?php
 // Enable error logging: 
 error_reporting(E_ALL ^ E_NOTICE);
 // mysqli connection via user-defined 
 
-
-
 include('./my_connect.php');
 $mysqli = get_mysqli_conn();
-
-
 
 // SQL statement
 $sql = "SELECT DISTINCT no_adults, no_child "
 . "FROM booking "
 . "WHERE booking.ID = ?";
-
 
 // SQL statement 2: updates booking based on number of adults
 $sql2 = "UPDATE booking "
@@ -30,16 +23,13 @@ $sql3 = "UPDATE booking "
 . "set no_children = ? "
 . "WHERE ID = ?";
 
-
 // Prepared statement, stage 1: prepare
 $stmt = $mysqli->prepare($sql);
-
 
 // Prepared statement, stage 2: bind and execute 
 $bID = $_POST['bID']; 
 $nadult = $_POST['nadult']; 
 $nchild = $_POST['nchild']; 
-
 
 // "i" for integer, "d" for double, "s" for string, "b" for blob 
 $stmt->bind_param('s', $bID);
@@ -48,12 +38,9 @@ $stmt->bind_param('i', $nchild);
 
 $stmt->execute();
 
-
 $stmt->bind_result($no_adults,$no_children);
 
-
 echo '<ul>';
-
 
 /* fetch values */ 
 if ($stmt->fetch()) { 
@@ -66,12 +53,10 @@ else {
 }
 echo '</ul>';
 
-
 /* close statement and connection*/ 
 $stmt->close(); 
 $stmt2->close(); 
 $stmt3->close(); 
 $mysqli->close();
-
 
 ?>
